dffabb56c4bae2af243fd0277b3b2ac787d6cf30
Sergey Efremov <sergey.efremov@smartbics.com>
2021-09-12 12:46:14 +0300

    Add gif's, add Exchanger and Phaser samples

---
+++ src/main/java/ru/gb/App10Exchanger.java
@@ -0,0 +1,71 @@
+package ru.gb;
+
+import java.util.concurrent.Exchanger;
+
+public class App10Exchanger {
+    // Exchanger (обменник) может понадобиться, для того, чтобы обменяться данными
+    // между двумя потоками в определенной точки работы обоих потоков. Обменник —
+    // обобщенный класс, он параметризируется типом объекта для передачи.
+
+    // Обменник является точкой синхронизации пары потоков: поток, вызывающий у
+    // обменника метод exchange() блокируется и ждет другой поток. Когда другой поток
+    // вызовет тот же метод, произойдет обмен объектами: каждая из них получит
+    // аргумент другой в методе exchange(). Стоит отметить, что обменник поддерживает
+    // передачу null значения. Это дает возможность использовать его для передачи объекта
+    // в одну сторону, или, просто как точку синхронизации двух потоков.
+
+    // Пример (https://habr.com/ru/post/277669/)
+    // Рассмотрим следующий пример. Есть два грузовика: один едет из пункта A в пункт D,
+    // другой из пункта B в пункт С. Дороги AD и BC пересекаются в пункте E. Из пунктов A
+    // и B нужно доставить посылки в пункты C и D. Для этого грузовики в пункте E должны
+    // встретиться и обменяться соответствующими посылками.
+
+    // Создаем обменник, который будет обмениваться типом String
+    private static final Exchanger<String> EXCHANGER = new Exchanger<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        // Формируем груз для 1-го грузовика
+        String[] p1 = new String[]{"{посылка A->D}", "{посылка A->C}"};
+        // Формируем груз для 2-го грузовика
+        String[] p2 = new String[]{"{посылка B->C}", "{посылка B->D}"};
+
+        //Отправляем 1-й грузовик из А в D
+        new Thread(new Truck(1, "A", "D", p1)).start();
+        Thread.sleep(100);
+
+        //Отправляем 2-й грузовик из В в С
+        new Thread(new Truck(2, "B", "C", p2)).start();
+    }
+
+    public static class Truck implements Runnable {
+        private final int number;
+        private final String dep;
+        private final String dest;
+        private final String[] parcels;
+
+        public Truck(int number, String departure, String destination, String[] parcels) {
+            this.number = number;
+            this.dep = departure;
+            this.dest = destination;
+            this.parcels = parcels;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.printf("В грузовик №%d погрузили: %s и %s.\n", number, parcels[0], parcels[1]);
+                System.out.printf("Грузовик №%d выехал из пункта %s в пункт %s.\n", number, dep, dest);
+                Thread.sleep(1000 + (long) Math.random() * 5000);
+                System.out.printf("Грузовик №%d приехал в пункт Е.\n", number);
+
+                // При вызове exchange() поток блокируется и ждет пока другой поток
+                // вызовет exchange(), после этого произойдет обмен посылками
+                parcels[1] = EXCHANGER.exchange(parcels[1]);
+                System.out.printf("В грузовик №%d переместили посылку для пункта %s.\n", number, dest);
+                Thread.sleep(1000 + (long) Math.random() * 5000);
+                System.out.printf("Грузовик №%d приехал в %s и доставил: %s и %s.\n", number, dest, parcels[0], parcels[1]);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/gb/App11Phaser.java
@@ -0,0 +1,132 @@
+package ru.gb;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Phaser;
+
+public class App11Phaser {
+    // Phaser (фазер), как и CyclicBarrier, является реализацией шаблона
+    // синхронизации Барьер, но, в отличии от CyclicBarrier, предоставляет больше
+    // гибкости. Этот класс позволяет синхронизировать потоки, представляющие отдельную
+    // фазу или стадию выполнения общего действия. Как и CyclicBarrier, Phaser является
+    // точкой синхронизации, в которой встречаются потоки-участники. Когда все стороны
+    // прибыли, Phaser переходит к следующей фазе и снова ожидает ее завершения.
+    //
+    // Если сравнить Phaser и CyclicBarrier, то можно выделить следующие важные
+    // особенности Phaser:
+    // - каждая фаза (цикл синхронизации) имеет номер;
+    // - количество сторон-участников жестко не задано и может меняться: поток
+    //   может регистрироваться в качестве участника и отменять свое участие;
+    // - участник не обязан ожидать, пока все остальные участники соберутся на
+    //   барьере. Чтобы продолжить свою работу достаточно сообщить о своем прибытии;
+    // - случайные свидетели могут следить за активностью в барьере;
+    // - поток может и не быть стороной-участником барьера, чтобы ожидать его
+    //   преодоления;
+    // - у фазера нет опционального действия.
+
+    // Пример (https://habr.com/ru/post/277669/)
+    //   Есть пять остановок. На первых четырех из них могут стоять пассажиры и
+    //   ждать автобуса. Автобус выезжает из парка и останавливается на каждой
+    //   остановке на некоторое время. После конечной остановки автобус едет в парк.
+    //   Нам нужно забрать пассажиров и высадить их на нужных остановках.
+
+    // Сразу регистрируем главный поток
+    private static final Phaser PHASER = new Phaser(1);
+
+    // Фазы 0 (START) и 6 (FINISH) - это автобусный парк, 1 - 5 остановки
+
+    public static final int START = 0;
+    public static final int FINISH = 6;
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Passenger> passengers = new ArrayList<>();
+
+        // Сгенерируем пассажиров на остановках
+        for (int i = 1; i < 5; i++) {
+            final Random random = new Random();
+            if (random.nextBoolean())
+                // Этот пассажир выходит на следующей
+                passengers.add(new Passenger(i, i + 1));
+
+            if (random.nextBoolean())
+                // Этот пассажир выходит на конечной
+                passengers.add(new Passenger(i, 5));
+        }
+
+        for (int i = 0; i < 7; i++) {
+            // int getPhase() — возвращает номер текущей фазы
+            int currentBusStop = PHASER.getPhase();
+            System.out.println("Остановка № " + currentBusStop);
+
+            switch (i) {
+                case START:
+                    System.out.println("Автобус выехал из парка.");
+                    // int arrive() — сообщает, что сторона завершила фазу, и
+                    // возвращает номер фазы. При вызове данного метода поток не
+                    // приостанавливается, а продолжает выполнятся
+                    PHASER.arrive(); // В фазе 0 всего 1 участник - автобус
+                    break;
+                case FINISH:
+                    System.out.println("Автобус уехал в парк.");
+                    // int arriveAndDeregister() — сообщает о завершении всех фаз
+                    // стороной и снимает ее с регистрации. Возвращает номер текущей
+                    // фазы
+                    PHASER.arriveAndDeregister(); // Снимаем главный поток, ломаем барьер
+                    break;
+                default:
+
+                    for (Passenger passenger : passengers)
+                        // Проверяем, есть ли пассажиры на остановке
+                        if (passenger.departure == currentBusStop) {
+                            // int register() — регистрирует нового участника,
+                            // который выполняет фазы. Возвращает номер текущей фазы;
+                            // Регистрируем поток, который будет участвовать в фазах
+                            PHASER.register();
+                            passenger.start();        // и запускаем
+                        }
+                    // int arriveAndAwaitAdvance() — указывает что поток завершил
+                    // выполнение фазы. Поток приостанавливается до момента, пока все
+                    // остальные стороны не закончат выполнять данную фазу. Точный
+                    // аналог CyclicBarrier.await(). Возвращает номер текущей фазы;
+                    PHASER.arriveAndAwaitAdvance(); // Сообщаем о своей готовности
+            }
+        }
+    }
+
+    public static class Passenger extends Thread {
+        private final int departure; // остановка, на которой пассажир садится в автобус
+        private final int destination; // остановка, на которой пассажир выходит
+
+        public Passenger(int departure, int destination) {
+            this.departure = departure;
+            this.destination = destination;
+            System.out.println(this + " ждёт на остановке № " + this.departure);
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println(this + " сел в автобус.");
+                // Пока автобус не приедет на нужную остановку (фазу)
+                while (PHASER.getPhase() < destination) {
+                    // заявляем в каждой фазе о готовности и ждем
+                    PHASER.arriveAndAwaitAdvance();
+                }
+                Thread.sleep(1);
+                System.out.println(this + " покинул автобус.");
+                // int arriveAndDeregister() — сообщает о завершении всех фаз
+                // стороной и снимает ее с регистрации. Возвращает номер текущей
+                // фазы
+                PHASER.arriveAndDeregister();   // Отменяем регистрацию на нужной фазе
+            } catch (InterruptedException e) {
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Пассажир{" + departure + " -> " + destination + '}';
+        }
+    }
+
+}
